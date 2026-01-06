1acda7e4321089f6c59ddddc639078f70d2eb48d
MrKirill <sklipiskirill@gmail.com>
2026-01-06 19:56:18 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/styles/fonts.css';
+import '@/styles/globals.css';
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <head>
+                <title>Lingoda</title>
+            </head>
+            <body>{children}</body>
+        </html>
+    );
+}
